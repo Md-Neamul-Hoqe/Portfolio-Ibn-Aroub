d5dbf415bbe91d5752d304c17889994c4a7f3de2
Md-Neamul-Hoqe <hoqe1997@gmail.com>
2024-03-27 19:32:48 +0600

    [update](experiences): Company name updated

--- src/pages/Home/Experiences/Experiences.jsx
+++ src/pages/Home/Experiences/Experiences.jsx
@@ -11,11 +11,9 @@ const Experiences = () => {
         <div className="flex max-md:flex-col">
           <div className="mb-4 flex-1">
             <h3 className="text-lg font-semibold">
-              Research Associate (Web Application Manager)
+              Web Developer & Database Administrator
             </h3>
-            <p className="text-xs">
-              Research Lab, Department of Mathematics, SUST.
-            </p>
+            <p className="text-xs">SUST Research Lab, SUST.</p>
             <p className="text-xs">July, 2023 - current</p>
             <p className="mt-2 text-main">
               Research Project: Under the research project ps\2023\1\19 <br />
@@ -26,9 +24,7 @@ const Experiences = () => {
 
           <div className="mb-4 flex-1">
             <h3 className="text-lg font-semibold">Research Student</h3>
-            <p className="text-xs">
-              Research Lab, Department of Mathematics, SUST.
-            </p>
+            <p className="text-xs">SUST Research Lab, SUST.</p>
             <p className="text-xs">July, 2022 - Jun, 2023</p>
             <p className="mt-2 text-main">
               Research Project: Under the research project ps\2022\1\20 <br />
